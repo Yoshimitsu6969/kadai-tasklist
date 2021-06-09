5597704f95968447c6db8de362d8e3ac7a295157
Yoshimitsu maegawa <panda.4432@icloud.com>
2021-06-09 21:37:30 +0000

    add seeder

--- database/migrations/2021_06_09_000840_add_status_to_tasks_table.php
+++ database/migrations/2021_06_09_000840_add_status_to_tasks_table.php
@@ -13,7 +13,7 @@ class AddStatusToTasksTable extends Migration
      */
     public function up()
     {
-        Schema::table('tasks', function (Blueprint $table) {
+        Schema::table('tasks', function (Blueprint $status) {
             $table->string("status");
         });
     }
@@ -25,7 +25,7 @@ class AddStatusToTasksTable extends Migration
      */
     public function down()
     {
-        Schema::table('tasks', function (Blueprint $table) {
+        Schema::table('tasks', function (Blueprint $status) {
             $table->dropColumn("status");
         });
     }
