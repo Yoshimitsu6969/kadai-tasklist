1f3371d432bdc5501b11d89c291af9547231891c
Yoshimitsu maegawa <panda.4432@icloud.com>
2021-06-09 09:24:32 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,11 +45,14 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $request->validate([
+            "status" =>"required|max:10",
             "content"=>'required|max:10',
+            
             ]);
         
         
         $task = new Task;
+        $task -> status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -103,12 +106,14 @@ class TasksController extends Controller
     {
         
             $request->validate([
+            "status" => 'required|max:10',     
             'content' => 'required|max:10',
         ]);
         
             
         $task = Task::findOrFail($id);
         // メッセージを更新
+        $task ->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
