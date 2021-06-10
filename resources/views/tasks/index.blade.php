9331f8af859b1af9482e4a60a4aaa13330a01489
Yoshimitsu maegawa <panda.4432@icloud.com>
2021-06-10 11:49:01 +0000

    Procfile

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -25,6 +25,7 @@
             </tbody>
         </table>
     @endif
-
+    {{-- ページネーションのリンク --}}
+    {{ $tasks->links() }}
 {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
 @endsection
